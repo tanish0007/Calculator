d2c9c540e08f1404a8c4af11b30db069255abd25
tanish0007 <jangratanish0007@gmail.com>
2025-06-23 06:34:10 +0530

    task done

--- js/script.js
+++ js/script.js
@@ -1,1 +1,45 @@
-const inputBox = document.querySelector("#inputBox");
+const INPUTBOX = document.querySelector("#inputBox");
+const NUMBERBOX = document.querySelector(".numberBox");
+const OPERATORBOX = document.querySelector(".operatorBox");
+const EQUALBTN = document.querySelector("#result");
+
+let op1;
+let operator;
+
+function createNumbers() {
+    for(let i = 0; i < 10; i++){
+        const digitBtn = document.createElement("button");
+        digitBtn.textContent = i;
+        NUMBERBOX.appendChild(digitBtn);
+        digitBtn.addEventListener("click", (event) => {
+            if(INPUTBOX.value == "0")
+                INPUTBOX.value = "";
+            INPUTBOX.value += event.target.textContent;
+        });
+    }
+}
+
+function createOperators() {
+    const operators = ["+", "-", "*", "/"];
+    operators.forEach((item) => {
+        const opBtn = document.createElement("button");
+        opBtn.textContent = item;
+        OPERATORBOX.appendChild(opBtn);
+        opBtn.addEventListener("click", (e) => {
+            op1 = parseInt(INPUTBOX.value);
+            console.log(op1);
+            INPUTBOX.value = "";
+            operator = e.target.textContent;
+        });
+    })
+}
+
+EQUALBTN.addEventListener("click", () => {
+    let result = op1 + operator + INPUTBOX.value;
+    INPUTBOX.value = eval(result);
+})
+
+
+
+createNumbers();
+createOperators();
